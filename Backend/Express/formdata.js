f4d238be449bbd70896e776cede3af1260332a39
Swastik Kulkarni <swas4kul@gmail.com>
2024-09-03 13:57:23 +0530

    first commit

--- Backend/Express/formdata.js
+++ Backend/Express/formdata.js
@@ -116,7 +116,7 @@ app.post('/about-form', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
